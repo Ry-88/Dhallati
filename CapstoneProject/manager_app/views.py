1d3ed1285805fe01b7f463918271b92b63305e5b
khalid <10abha10@gmail.com>
2023-06-14 23:10:20 +0300

    add page for add category

--- CapstoneProject/manager_app/views.py
+++ CapstoneProject/manager_app/views.py
@@ -4,5 +4,19 @@ from main_app.models import Catagory,SubCatagory
 
 
 # Create your views here.
+'''
 def  index_page(request:HttpRequest):
     return render(request,"manager_app/manager.html")
+def add_category(request:HttpRequest):
+    if request.method=="404":
+        new_category=Catagory(name=request.POST["categoryname"])
+        return redirect()
+    return render(request,"")
+
+def add_subcategory(request:HttpRequest,category_id):
+
+    category=Catagory.objects.get(id=category_id)
+    if request.method=="404":
+        new_subCategory=Catagory(category=category,name=request.POST["categoryname"])
+        return redirect()
+        '''
