c98d22661cae55a0fca785bdfaa6f04151a3864b
Abdulaziz <Abdulaziz-A1997@hotmail.com>
2023-06-14 11:20:02 +0300

    accounts app created with views and urls

---
+++ CapstoneProject/accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+app_name = 'accounts'
+
+urlpatterns = [
+    path('log_in', views.log_in, name='log_in'),
+    path('log_out', views.log_out, name='log_out'),
+    path('no_permission', views.no_permission, name='no_permission')
+]
---
+++ CapstoneProject/accounts/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+from django.http import HttpRequest
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+
+# Create your views here.
+
+def log_in(request:HttpRequest):
+
+    msg = None
+
+    if request.method == 'POST':
+        user : User = authenticate(request, username=request.POST['username'], password=request.POST["password"])
+        login(request, user)
+        return redirect("main_app:home")
+    else:
+        msg = 'username or password is incorrect'
+
+    return render(request, 'accounts/log_in.html', {'msg' : msg})
+
+
+def log_out(request:HttpRequest):
+
+    logout(request)
+
+
+    return redirect('main_app:home')
+
+
+def no_permission(request:HttpRequest):
+
+
+
+    return render(request, 'accounts/no_permission.html')
