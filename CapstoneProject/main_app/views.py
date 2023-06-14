d9bd00229cb4c11410976d14510e433a34650468
Abdulaziz <Abdulaziz-A1997@hotmail.com>
2023-06-14 13:33:14 +0300

    added main app pages with views and urls

--- CapstoneProject/main_app/views.py
+++ CapstoneProject/main_app/views.py
@@ -1,2 +1,38 @@
 from django.shortcuts import render, redirect
+from django.http import HttpRequest
 # Create your views here.
+
+
+
+def home(request:HttpRequest):
+
+
+    return render(request, 'main_app/home.html')
+
+
+def not_found(request:HttpRequest):
+
+
+
+    return render(request, 'main_app/not_found.html')
+
+
+def request_add(request:HttpRequest):
+
+
+
+    return render(request, 'main_app/request_add.html')
+
+
+def request_detail(request:HttpRequest):
+
+
+
+    return render(request, 'main_app/request_detail.html')
+
+
+def request_tracking(request:HttpRequest):
+
+
+
+    return render(request, 'main_app/request_tracking.html')
