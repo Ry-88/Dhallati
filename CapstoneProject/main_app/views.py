d369d38b41d8217af64ce21f8c48fdd41eb53cad
Alaa <alaabinothman93@gmail.com>
2023-06-15 11:51:40 +0300

    added SubCatagory

--- CapstoneProject/main_app/views.py
+++ CapstoneProject/main_app/views.py
@@ -1,54 +1,45 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest
-from .models import RequestLostItem,Catagory,SubCatagory
+from .models import RequestLostItem, Catagory, SubCatagory
 # Create your views here.
 
 
 
-def home(request:HttpRequest):
-
+def home(request: HttpRequest):
 
     return render(request, 'main_app/home.html')
 
 
-def not_found(request:HttpRequest):
-
-
+def not_found(request: HttpRequest):
 
     return render(request, 'main_app/not_found.html')
 
 
-def request_add(request:HttpRequest):
+def request_add(request: HttpRequest):
 
     catagories = Catagory.objects.all()
-
+    sub_catagory = SubCatagory.objects.filter()
     if request.method == "POST":
-        catagory = Catagory.objects.get(id=request.POST["catagory"])
-    if "image" in request.FILES:
-        new_request = RequestLostItem(catagory=request.POST["catagory"],color=request.POST["color"] ,place=request.POST["place"] ,
-                            description=request.POST["description"],image=request.FILES["image"],
-                            status = request.POST["status"],is_read = request.POST["is_read"],
-                            created_at=request.POST["created_at"])
-    else:
-        new_request = RequestLostItem(catagory=request.POST["catagory"],color=request.POST["color"] ,place=request.POST["place"] ,
-                            description=request.POST["description"],
-                            status = request.POST["status"],is_read = request.POST["is_read"],
-                            created_at=request.POST["created_at"])
+        if "image" in request.FILES:
+            new_request = RequestLostItem(catagory=request.POST["catagory"], color=request.POST["color"], 
+                                          place=request.POST["place"], description=request.POST["description"], 
+                                          image=request.FILES["image"],status=request.POST["status"], 
+                                          is_read=request.POST["is_read"])
+        else:
+            new_request = RequestLostItem(catagory=request.POST["catagory"], color=request.POST["color"], 
+                                          place=request.POST["place"],description=request.POST["description"],
+                                          status=request.POST["status"], is_read=request.POST["is_read"])
         new_request.save()
         return redirect("main_app:home")
 
-    return render(request, 'main_app/request_add.html',{"catagories" : catagories})
+    return render(request, 'main_app/request_add.html', {"catagories": catagories})
 
 
-def request_detail(request:HttpRequest):
-
-
+def request_detail(request: HttpRequest):
 
     return render(request, 'main_app/request_detail.html')
 
 
-def request_tracking(request:HttpRequest):
-
-
+def request_tracking(request: HttpRequest):
 
     return render(request, 'main_app/request_tracking.html')
