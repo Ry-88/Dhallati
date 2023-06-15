9e2c54c1eebace9aadbcfa8a4d7c9f0cae009642
Abdulaziz <Abdulaziz-A1997@hotmail.com>
2023-06-15 11:52:49 +0300

    added the backend for home page

--- CapstoneProject/main_app/views.py
+++ CapstoneProject/main_app/views.py
@@ -7,6 +7,20 @@ from django.http import HttpRequest
 def home(request:HttpRequest):
 
 
+
+    if request.method == 'POST':
+
+        if 'track' in request.POST:
+            all_track = Class.objects.all()
+            track_request= Class.objects.get(id=request.POST['request_number'])
+
+        if 'help' in request.POST:
+            new_help = Class(first_name=request.POST['first_name'],
+                             last_name=request.POST['last_name'],
+                             email=request.POST['email'],
+                             message=request.POST['message'])
+            new_help.save()
+
     return render(request, 'main_app/home.html')
 
 
@@ -24,8 +38,9 @@ def request_add(request:HttpRequest):
     return render(request, 'main_app/request_add.html')
 
 
-def request_detail(request:HttpRequest):
+def request_detail(request:HttpRequest, request_id):
 
+    
 
 
     return render(request, 'main_app/request_detail.html')
