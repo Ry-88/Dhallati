e0c73cc5fc2fc197b6047502b6e253dfd74899b6
khalid <10abha10@gmail.com>
2023-06-16 11:08:32 +0300

    add category(add ,delete)func and subgategory also

--- CapstoneProject/manager_app/urls.py
+++ CapstoneProject/manager_app/urls.py
@@ -5,8 +5,14 @@ app_name = "manager_app"
 
 urlpatterns = [
     path('',views.index_page,name="index_page"),
+    path("category",views.category_page,name="category_page" ),
+    path('category/delete/<category_id>/',views.delete_category,name="delete_category"),
+    path('category/subcategory/<category_id>/',views.sub_category,name="sub_category"),
+    path('category/subcategory/<category_id>/delete/<sub_category_id>/',views.delete_sub_category,name="delete_sub_category"),
 
-    path('add/category/',views.add_category,name="add_category"),
-    path('add/category/<category_id>/subcategory',views.add_subcategory,name="add_subcategory")
+
+    path("founditem/add/",views.add_found_item_page,name="add_found_item_page"),
+
+
     
 ]
--- CapstoneProject/manager_app/views.py
+++ CapstoneProject/manager_app/views.py
@@ -2,26 +2,41 @@ from django.shortcuts import render,redirect
 from django.http import HttpRequest,HttpResponse
 from main_app.models import Catagory,SubCatagory
 
+from django.core import serializers
+import json
 
 # Create your views here.
 
-def  index_page(request:HttpRequest):
+#base-file-for-exdends---------------------------
+def  index_page(request:HttpRequest): 
+    return render(request,"manager_app/manager.html")
+#-------------------------------------
+
+
+def category_page(request:HttpRequest):
     Catagorys=Catagory.objects.all()
-    return render(request,"manager_app/manager.html",{"Catagorys":Catagorys})
-
-
-def add_category(request:HttpRequest):
+    #for add new category
     if request.method=="POST":
 
         new_category=Catagory(name=request.POST["categoryname"])
         new_category.save()
-        return redirect('manager_app:index_page')
-    return render(request,"manager_app/add_category_page.html")
+        return redirect('manager_app:category_page')
+    
+
+    return render(request,"manager_app/category.html",{"Catagorys":Catagorys})
 
 
 
 
-def add_subcategory(request:HttpRequest,category_id):
+def delete_category(request:HttpRequest,category_id):
+ 
+    category=Catagory.objects.get(id=category_id)
+    category.delete()
+    return redirect("manager_app:category_page")
+
+#add_subcategory
+
+def sub_category(request:HttpRequest,category_id):
 
     category=Catagory.objects.get(id=category_id)
     
@@ -29,6 +44,26 @@ def add_subcategory(request:HttpRequest,category_id):
         new_subCategory=SubCatagory(category=category,name=request.POST["categoryname"])
         new_subCategory.save()
 
-        return redirect('manager_app:add_subcategory',category_id)
+        return redirect('manager_app:sub_category',category_id)
     sub_category=SubCatagory.objects.filter(category=category)
-    return render(request,"manager_app/add_subcategory_page.html" ,{"category":category, "sub_category":sub_category})
+    return render(request,"manager_app/sub_category_page.html" ,{"category":category, "sub_category":sub_category})
+
+
+
+
+def delete_sub_category(request:HttpRequest,category_id,sub_category_id):
+   
+    sub_category=SubCatagory.objects.get(id=sub_category_id)
+    sub_category.delete()
+    return redirect('manager_app:sub_category',category_id)
+
+def add_found_item_page(request:HttpRequest):
+    catagorys=Catagory.objects.all()
+    sub_category=SubCatagory.objects.all()
+
+
+    return render(request,"manager_app/add_found_item_page.html",{"catagorys":catagorys,"sub_category":sub_category})
+    
+
+
+
