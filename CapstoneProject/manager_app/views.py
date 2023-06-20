9a7a100a7040c086824d43747ce4e6f332d932dc
khalid <10abha10@gmail.com>
2023-06-20 18:28:16 +0300

    add delete

--- CapstoneProject/manager_app/views.py
+++ CapstoneProject/manager_app/views.py
@@ -114,7 +114,11 @@ def found_item_page(request:HttpRequest):
     
     return render(request,"manager_app/found_items.html",{"found_items":found_items,"bell":bell})
 
-
+def delete_found_item(request:HttpRequest,found_item_id):
+    found_item=FoundItem.objects.get(id=found_item_id)
+    found_item.delete()
+    redirect("manager_app:found_item_page")
+    
 
 def found_detail_page(request:HttpRequest,found_item_id):
     
